e91104db121ae94e6a8eca85ad327320fdee3002
lookslikeitsnot <cedr2009@gmail.com>
2023-05-16 00:34:12 +0200

    add storing test and logic when --store-failures

--- macros/schema_tests/_generalized/expression_is_true.sql
+++ macros/schema_tests/_generalized/expression_is_true.sql
@@ -19,15 +19,31 @@
 {%- endmacro %}
 
 {% macro default__expression_is_true(model, expression, test_condition, group_by_columns, row_condition) -%}
+    {% set aggregations_pattern = '(avg\(.*\))|(count\(.*\))|(max\(.*\))|(min\(.*\))|(sum\(.*\))|(stddev\(.*\))' %}
+    {% set re = modules.re %}
+    {% set inline_expression = re.sub('\s{2,}|\n', " ", expression) %}
+    {% set is_aggregation = re.search(aggregations_pattern, inline_expression, re.IGNORECASE) %}
+    {%- if should_store_failures() -%}
+    {{ exceptions.warn(
+            "expression : " ~ inline_expression
+    ) }}
+    {{ log('group by columns: ' ~ group_by_columns)}}
+    {{ log('is aggregation: ' ~ is_aggregation)}}
+    {% endif %}
 with grouped_expression as (
     select
         {% if group_by_columns %}
         {% for group_by_column in group_by_columns -%}
         {{ group_by_column }} as col_{{ loop.index }},
         {% endfor -%}
+        {# if is_aggregation, don not get any extra information #}
+        {% elif is_aggregation %}
+        {# if storing failures, store full model if not grouping #}
+        {% elif should_store_failures() %}
+        model_.*,
         {% endif %}
         {{ dbt_expectations.truth_expression(expression) }}
-    from {{ model }}
+    from {{ model }} model_
      {%- if row_condition %}
     where
         {{ row_condition }}
